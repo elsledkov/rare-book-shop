03b1c63d94e8501d626ca38f3fdb29f1d52a1a04
Evgeny Sledkov <elsledkov@gmail.com>
2025-02-06 18:36:56 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Rare book shop project greets you!");
+        System.out.println("string 2");
     }
 }
